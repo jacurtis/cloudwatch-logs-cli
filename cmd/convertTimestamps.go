b01758d181896f7f9ad0a5b7423eb924b33d7125
Alex Curtis <alex.curtis@benchmarkanalytic.com>
2021-12-30 18:38:41 -0700

    Add ability to rename file, or by default override previous file

--- cmd/convertTimestamps.go
+++ cmd/convertTimestamps.go
@@ -15,16 +15,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// convertTimestampsCmd represents the convertTimestamps command
+// convertTimestampsCmd represents the convert-timestamps command
 var convertTimestampsCmd = &cobra.Command{
 	Use:   "convert-timestamps",
 	Short: "Convert the timestamps column in cloudwatch logs into a human readable date",
 	Long: `CloudWatch Log exports will by default provide the first column with UNIX Timestamps
 	instead of dates. Since UNIX timestamps are not human readable, this command will convert
 	into something that is easier to parse for humans. 
-	For example:
 
-Example here`,
+	Usage:
+
+	cloudwatch-logs convert-timestamps [file] -z [timezone]
+	cloudwatch-logs convert-timestamps myfile.csv -z America/Chicago
+`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Process CSV
 		file, err := os.Open(args[0])
@@ -48,32 +51,38 @@ Example here`,
 		for _, record := range records {
 			i, err := strconv.ParseInt(record[0], 10, 64)
 			if err != nil {
-				log.Fatal(err)
+				fmt.Println("Could not parse timestamps. Are they already converted?")
+				fmt.Println("Check the file and run again.")
+				os.Exit(1)
 			}
 			tm := time.UnixMilli(i)
 			timezone, _ := cmd.Flags().GetString("timezone")
 			tz, _ := time.LoadLocation(timezone)
-			fmt.Println(tm.In(tz).Format(time.RFC1123))
+			record[0] = tm.In(tz).Format(time.RFC1123)
+		}
+		fmt.Printf("Successfully converted %s records\n", strconv.Itoa(len(records)))
+		fmt.Println("Saving to file...")
+
+		newFilename, _ := cmd.Flags().GetString("rename")
+		if newFilename == "" {
+			newFilename = args[0]
 		}
 
-		// for {
-		// 	row, err := reader.Read()
-
-		// 	if err == io.EOF {
-		// 		break
-		// 	}
-
-		// 	if err != nil {
-		// 		log.Fatal(err)
-		// 	}
-		// 	fmt.Println(row[0])
-		// for col := range row {
-		// 	fmt.Printf("%s\n", row[col])
-		// }
-		// Loop through each line
-		// Convert the timestamp to date
 		// Save the file
+		newFile, err := os.Create(newFilename)
+		defer newFile.Close()
+		if err != nil {
+			fmt.Println("Failed to create new file")
+			log.Fatal(err)
+		}
+		writer := csv.NewWriter(newFile)
+		err = writer.WriteAll(records)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		// Print something to the user
+		fmt.Println("File is updated and ready to share!")
 	},
 }
 
@@ -88,5 +97,6 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
+	convertTimestampsCmd.Flags().StringP("rename", "n", "", "Rename the file to this name (add .csv to end)")
 	convertTimestampsCmd.Flags().StringP("timezone", "z", "America/Chicago", "Timezone to convert into")
 }
